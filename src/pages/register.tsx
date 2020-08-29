4b335246f7fb2947566039cb901af4eb5525600b
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-29 23:12:31 +0200

    basic urql to connect front and back ends

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,15 +10,29 @@ import {
 } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from 'urql';
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `
+    mutation Register($username: String!, $password: String!) {
+      register(options: { username: $username, password: $password }) {
+        errors { field message }
+        user { id username createdAt updatedAt }
+      }
+    }
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [{}, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => {
+          // console.log(values);
+          return register(values);
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
@@ -37,7 +51,6 @@ const Register: React.FC<registerProps> = ({}) => {
             </Box>
             <Button
               mt={4}
-              //   mx='auto'
               type='submit'
               isLoading={isSubmitting}
               variantColor='teal'
