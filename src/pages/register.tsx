795bd40222c7cf6cb9b04132a3831e947770aaf8
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-30 12:43:11 +0200

    redirecting on successful register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -12,11 +12,13 @@ import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-  const [{}, register] = useRegisterMutation();
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant='small'>
       <Formik
@@ -26,6 +28,8 @@ const Register: React.FC<registerProps> = ({}) => {
           console.log({ response });
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push('/');
           }
         }}
       >
