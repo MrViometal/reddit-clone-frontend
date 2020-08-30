92eb496ccee4144bb71d774b4d9a5f0786e52324
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-30 13:37:24 +0200

    basic navbar and checking cookie

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Box, Link, Flex, Button } from '@chakra-ui/core';
+import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }, me] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    //data is loading
+  } else if (!data?.me) {
+    //user not logged in
+    body = (
+      <>
+        <NextLink href='/login'>
+          <Link color='white' mr={2}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href='/register'>
+          <Link color='white' mr={2}>
+            Register
+          </Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    //use is logged in
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant='link'>Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg='tomato' p={4}>
+      <Box ml={'auto'}>{body}</Box>
+    </Flex>
+  );
+};
