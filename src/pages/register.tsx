8b6d69ee321f2381d5fc2df2ae83d79b63b6f2a8
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-30 02:19:19 +0200

    register user through front end...check server problem, and cookie not setting

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,28 +10,23 @@ import {
 } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
-import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface registerProps {}
 
-const REGISTER_MUTATION = `
-    mutation Register($username: String!, $password: String!) {
-      register(options: { username: $username, password: $password }) {
-        errors { field message }
-        user { id username createdAt updatedAt }
-      }
-    }
-`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [{}, register] = useMutation(REGISTER_MUTATION);
+  const [{}, register] = useRegisterMutation();
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={values => {
-          // console.log(values);
-          return register(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          console.log({ response });
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          }
         }}
       >
         {({ isSubmitting }) => (
