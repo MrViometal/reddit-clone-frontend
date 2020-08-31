404ef2e8d5661b134c86bca961cc838c6cd0c836
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-31 14:33:15 +0200

    server side rendering

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Box, Link, Flex, Button } from '@chakra-ui/core';
 import NextLink from 'next/link';
 import { useMeQuery, useLogoutMutation } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  const [{ data, fetching: meFetching }] = useMeQuery();
+  const [{ data, fetching: meFetching }] = useMeQuery({
+    pause: isServer(),
+  });
   let body = null;
 
   if (meFetching) {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,74 +11,74 @@ import {
   LogoutMutation,
 } from '../generated/graphql';
 
-function betterUpdateQuery<Result, Query>(
-  cache: Cache,
-  queryInput: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query,
-) {
-  return cache.updateQuery(queryInput, data => fn(result, data as any) as any);
-}
+// function betterUpdateQuery<Result, Query>(
+//   cache: Cache,
+//   queryInput: QueryInput,
+//   result: any,
+//   fn: (r: Result, q: Query) => Query,
+// ) {
+//   return cache.updateQuery(queryInput, data => fn(result, data as any) as any);
+// }
 
-const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  fetchOptions: {
-    credentials: 'include',
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          login: (_result, args, cache, info) => {
-            // cache.updateQuery({ query: MeDocument });
-            betterUpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) return query;
-                else return { me: result.login.user };
-              },
-            );
-          },
-          register: (_result, args, cache, info) => {
-            // cache.updateQuery({ query: MeDocument });
-            betterUpdateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) return query;
-                else return { me: result.register.user };
-              },
-            );
-          },
-          logout: (_result, args, cache, info) => {
-            betterUpdateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null }),
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
+// const client = createClient({
+//   url: 'http://localhost:4000/graphql',
+//   fetchOptions: {
+//     credentials: 'include',
+//   },
+//   exchanges: [
+//     dedupExchange,
+//     cacheExchange({
+//       updates: {
+//         Mutation: {
+//           login: (_result, args, cache, info) => {
+//             // cache.updateQuery({ query: MeDocument });
+//             betterUpdateQuery<LoginMutation, MeQuery>(
+//               cache,
+//               { query: MeDocument },
+//               _result,
+//               (result, query) => {
+//                 if (result.login.errors) return query;
+//                 else return { me: result.login.user };
+//               },
+//             );
+//           },
+//           register: (_result, args, cache, info) => {
+//             // cache.updateQuery({ query: MeDocument });
+//             betterUpdateQuery<RegisterMutation, MeQuery>(
+//               cache,
+//               { query: MeDocument },
+//               _result,
+//               (result, query) => {
+//                 if (result.register.errors) return query;
+//                 else return { me: result.register.user };
+//               },
+//             );
+//           },
+//           logout: (_result, args, cache, info) => {
+//             betterUpdateQuery<LogoutMutation, MeQuery>(
+//               cache,
+//               { query: MeDocument },
+//               _result,
+//               () => ({ me: null }),
+//             );
+//           },
+//         },
+//       },
+//     }),
+//     fetchExchange,
+//   ],
+// });
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ThemeProvider>
-    </Provider>
+    // <Provider value={client}>
+    <ThemeProvider theme={theme}>
+      {/* <ColorModeProvider> */}
+      <CSSReset />
+      <Component {...pageProps} />
+      {/* </ColorModeProvider> */}
+    </ThemeProvider>
+    // </Provider>
   );
 }
 
