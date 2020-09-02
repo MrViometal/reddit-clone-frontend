229e51dff85351bd7ce4cfc8972c55caba884073
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-09-02 15:26:21 +0200

    pagination FINALLY

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,59 @@
-import { Box, Link } from '@chakra-ui/core';
+import { Box, Link, Stack, Heading, Text, Flex, Button } from '@chakra-ui/core';
 import { withUrqlClient } from 'next-urql';
 import NextLink from 'next/link';
 import Layout from '../components/Layout';
 import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
+import { useState } from 'react';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({ variables: { limit: 10 } });
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
+  });
+  const [{ data, fetching }] = usePostsQuery({ variables });
+
+  if (!fetching && !data) return <div>you have something wrong</div>;
+
   return (
     <Layout>
-      <Box style={{ color: 'green' }}>
-        <NextLink href='/create-post'>
-          <Link>Create Post</Link>
-        </NextLink>
-      </Box>
+      <Flex align='center'>
+        <Heading>LiReddit</Heading>
+        <Box ml='auto' style={{ color: 'green' }}>
+          <NextLink href='/create-post'>
+            <Link>Create Post</Link>
+          </NextLink>
+        </Box>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
-        data.posts.map(p => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data!.posts.posts.map(p => (
+            <Box key={p.id} p={5} shadow='md' borderWidth='1px'>
+              <Heading fontSize='xl'>{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data && data.posts.hasMore && (
+        <Flex>
+          <Button
+            onClick={() =>
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              })
+            }
+            isLoading={fetching}
+            m='auto'
+            my={8}
+          >
+            Load more...
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
