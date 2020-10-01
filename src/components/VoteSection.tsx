12d91a6886f0fbda6d096cffbd3966204898de2f
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-10-01 23:11:14 +0200

    add voteStatus and colors to up and down votes buttons

--- src/components/VoteSection.tsx
+++ src/components/VoteSection.tsx
@@ -14,13 +14,16 @@ const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
   const [loadingState, setLoadingState] = useState<
     'up-vote' | 'down-vote' | 'not-loading'
   >('not-loading');
-  const [{ fetching, operation }, vote] = useVoteMutation();
+  const [, vote] = useVoteMutation();
   return (
     <Flex direction='column' alignItems='center' justifyContent='center' mr={4}>
       <IconButton
         icon='chevron-up'
         aria-label='upVote post'
+        variantColor={post.voteStatus === 1 ? 'green' : undefined}
         onClick={async () => {
+          if (post.voteStatus === 1) return;
+
           setLoadingState('up-vote');
           await vote({
             postId: post.id,
@@ -34,7 +37,9 @@ const VoteSection: React.FC<VoteSectionProps> = ({ post }) => {
       <IconButton
         icon='chevron-down'
         aria-label='downVote post'
+        variantColor={post.voteStatus === -1 ? 'red' : undefined}
         onClick={async () => {
+          if (post.voteStatus === -1) return;
           setLoadingState('down-vote');
           await vote({
             postId: post.id,
